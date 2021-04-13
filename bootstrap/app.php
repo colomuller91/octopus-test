42c648a7581cf67ff52fddd670d316fcae9d528b
Sebastian Portesi <sportesi@octopus.com.ar>
2021-04-13 11:43:10 -0300

    Rompi todo

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,8 +27,8 @@ $app = new Illuminate\Foundation\Application(
 */
 
 $app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
+    Illuminate\Contracts\Http\Kernel::class
+    // App\Http\Kernel::class
 );
 
 $app->singleton(
